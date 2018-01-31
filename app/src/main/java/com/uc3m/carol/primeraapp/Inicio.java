ce456c853cdea6ec686554546fedaa131e067e99
Calorain <35992877+Calorain@users.noreply.github.com>
2018-01-31 10:31:14 +0100

    Initial commit

---
+++ app/src/main/java/com/uc3m/carol/primeraapp/Inicio.java
@@ -0,0 +1,13 @@
+package com.uc3m.carol.primeraapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Inicio extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+    }
+}
